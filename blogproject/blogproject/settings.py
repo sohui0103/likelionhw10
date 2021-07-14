669034b1988091c846b50a276272218705e63ecb
sohui0103 <sohui0103@naver.com>
2021-07-14 16:56:15 +0900

    setting.py 수정1

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KYE','django-insecure-*d21wxb$66=-^^-c$c@i0lif_&szceta+ii%8-4vsm!j$4*fw7')
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-*d21wxb$66=-^^-c$c@i0lif_&szceta+ii%8-4vsm!j$4*fw7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,4 +128,9 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
- 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'AKIAZ4OGVNEN7KFX3VDY'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'kimsohui'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
